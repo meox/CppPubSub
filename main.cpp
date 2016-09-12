d7d84139af8264d852cb4b64884a43c6bf547c34
Gian Lorenzo Meocci <gianlorenzo.meocci@commprove.com>
2016-09-12 08:20:38 +0200

    fix indents

--- main.cpp
+++ main.cpp
@@ -7,15 +7,15 @@ using namespace ps;
 
 struct global_sub : Subscriber<std::string*>
 {
-    void execute(topic_raw_ptr topic, data_t data) override
-    {
-        counter++;
+	void execute(topic_raw_ptr topic, data_t data) override
+	{
+		counter++;
 
 		if (counter == 10)
 			std::cout << *data << "\n";
-    }
+	}
 
-    size_t counter{0};
+	size_t counter{0};
 };
 
 
@@ -35,51 +35,51 @@ std::string* push_data(const std::string& e)
 int main()
 {
 	auto meteo = create_topic<std::string*>("meteo");
-    auto meteo_station = create_publisher(meteo);
+	auto meteo_station = create_publisher(meteo);
 
-    auto temp = create_topic<std::string*>("temp");
-    auto season = create_publisher(temp);
+	auto temp = create_topic<std::string*>("temp");
+	auto season = create_publisher(temp);
 
 	auto snd = create_publisher<std::string*, custom_publisher>(temp);
 
-    size_t web_news_counter{};
-    auto web_news = create_subscriber(meteo, [&web_news_counter](const Topic<std::string*>* topic, const std::string* data){
-        web_news_counter++;
-    });
+	size_t web_news_counter{};
+	auto web_news = create_subscriber(meteo, [&web_news_counter](const Topic<std::string*>* topic, const std::string* data){
+		web_news_counter++;
+	});
 
-    size_t ansa_counter{};
-    auto ansa = create_subscriber(meteo, [&ansa_counter](const Topic<std::string*>* topic, const std::string* data){
-        ansa_counter++;
-    });
+	size_t ansa_counter{};
+	auto ansa = create_subscriber(meteo, [&ansa_counter](const Topic<std::string*>* topic, const std::string* data){
+		ansa_counter++;
+	});
 
 
-    global_sub global;
-    global.subscribe({meteo, temp});
+	global_sub global;
+	global.subscribe({meteo, temp});
 
-    web_news->run();
-    ansa->run();
-    global.run();
+	web_news->run();
+	ansa->run();
+	global.run();
 
-    std::thread th_meteo([&]{
-        std::vector<std::string> cities{"Rome", "Florence", "Venice"};
+	std::thread th_meteo([&]{
+		std::vector<std::string> cities{"Rome", "Florence", "Venice"};
 
-        for (uint32_t i = 0; i < 1000000; i++)
-        {
+		for (uint32_t i = 0; i < 1000000; i++)
+		{
 			const std::string tcelsius = std::to_string(24 + (i % 10));
-            meteo_station->produce(push_data(tcelsius));
-            season->produce(push_data(cities[i%3] + ", " + tcelsius));
-        }
-    });
+			meteo_station->produce(push_data(tcelsius));
+			season->produce(push_data(cities[i%3] + ", " + tcelsius));
+		}
+	});
 
-    th_meteo.join();
+	th_meteo.join();
 
-    web_news->unsubscribe();
-    ansa->unsubscribe();
-    global.unsubscribe();
+	web_news->unsubscribe();
+	ansa->unsubscribe();
+	global.unsubscribe();
 
-    std::cout << "web_news_counter: " << web_news_counter << std::endl;
-    std::cout << "ansa_counter: " << ansa_counter << std::endl;
-    std::cout << "g_counter: " << global.counter << std::endl;
+	std::cout << "web_news_counter: " << web_news_counter << std::endl;
+	std::cout << "ansa_counter: " << ansa_counter << std::endl;
+	std::cout << "g_counter: " << global.counter << std::endl;
 
-    return 0;
+	return 0;
 }
